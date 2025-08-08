19929da2f4c1ab75aadd2debbb26bf9326f3bf05
serii <seriiburduja@gmail.com>
2025-08-08 21:04:05 +0300

    feat: delete ruff

--- FilesHanlde.py
+++ FilesHanlde.py
@@ -1,120 +1,115 @@
 import os
-from datetime import datetime
-from rich import print
+from pathlib import Path
 
-from MyTable import MyTable
+from rich import print
+from simple_term_menu import TerminalMenu
+
+from InputValidator import InputValidator
+from Print import Print
 from Select import Select
 
 
 class FilesHandle:
-    def __init__(self, basepath: str):
-        self.basepath = basepath if basepath != "" else "."
+    def list_files(self, path_to_list, file_extension=None) -> None:
+        abs_path = Path(path_to_list).resolve()
+        print(f"[green]Listing files in ================ {abs_path}")
 
-    def listFiles(self, path_to_list=""):
-        if path_to_list:
-            self.basepath = path_to_list
-        files = []
-        for entry in os.listdir(self.basepath):
-            if os.path.isfile(os.path.join(self.basepath, entry)):
-                files.append([len(files) + 1, entry])
-        MyTable.show("Files", ["Id", "File name"], files)
+        files = [f for f in abs_path.iterdir() if f.is_file()]
+        if not files:
+            print("[yellow]No files found in this directory.")
+            return
+        sorted_files = sorted(files, key=lambda f: f.name.lower())
 
-    def listDir(self, path_to_list=""):
-        directories = []
-        if path_to_list:
-            self.basepath = path_to_list
-        with os.scandir(self.basepath) as entries:
-            for entry in entries:
+        for f in sorted_files:
+            if file_extension:
+                if f.name.endswith(file_extension):
+                    print(f.name)
+            else:
+                print(f.name)
+
+    def list_dir(self, path_to_list=""):
+        print(f"[blue]Listing directories in ================ {path_to_list}")
+        abs_path = Path(path_to_list).resolve()
+        with os.scandir(abs_path) as entries:
+            sorted_dirs = sorted(
+                (entry for entry in entries if entry.is_dir()),
+                key=lambda e: e.name.lower(),
+            )
+            for entry in sorted_dirs:
                 if entry.is_dir():
-                    directories.append(entry.name)
-        directories.sort()
-        tb = MyTable()
-        tb.show(
-            "View Folders",
-            ["Id", "Directory name"],
-            [[i + 1, dir_name] for i, dir_name in enumerate(directories)],
-        )
+                    print(entry.name)
 
-    def createOrChooseDirectory(self, path_to_dir=""):
-        if path_to_dir:
-            self.basepath = path_to_dir
-        else:
-            self.basepath = os.getcwd()
-        self.listDir(self.basepath)
+    def create_or_choose_directory(self, path_to_dir="") -> str:
+        abs_path = str(Path(path_to_dir).resolve())
+        if not os.path.exists(abs_path):
+            os.makedirs(abs_path)
+
+        self.list_dir(abs_path)
+
+        if not self._has_dirs(path_to_dir):
+            return self._create_dir(abs_path)
+
         select_or_create = Select.select_one(["Select", "Create"])
         if select_or_create == "Create":
-            dir_name = input("Enter directory name:")
-            if dir_name == "":
-                print("Directory name is required")
-                exit()
-            else:
-                os.makedirs(self.basepath + "/" + dir_name)
-                print("Directory created")
-                return dir_name
+            return self._create_dir(abs_path)
         else:
-            selected_dir = self.chooseDir()
-            return selected_dir
+            selected_dir = self.choose_dir(abs_path)
+            return str(Path(path_to_dir) / selected_dir[0])
 
-    def chooseDir(self):
-        choosed_dir = []
-        with os.scandir(self.basepath) as entries:
+    def _create_dir(self, abs_path):
+        dir_name = InputValidator.get_string("Enter directory name: ")
+        current_path = str(Path(abs_path) / dir_name)
+        os.makedirs(current_path)
+        print("Directory created")
+        return current_path
+
+    def choose_dir(self, path_to_dir):
+        chosen_dir = []
+        abs_path = Path(path_to_dir).resolve()
+        with os.scandir(abs_path) as entries:
             for entry in entries:
                 if entry.is_dir():
-                    choosed_dir.append(entry.name)
-        choosed_dir.sort()
-        selected_dir = Select.select_with_fzf(choosed_dir)
+                    chosen_dir.append(entry.name)
+        chosen_dir.sort()
+        selected_dir = Select.select_with_fzf(chosen_dir)
         return selected_dir
 
-    def listFilesWithPrefix(self, prefix):
-        print(f"Listing directories in ================ {self.basepath}")
-        for entry in os.listdir(self.basepath):
-            if os.path.isfile(os.path.join(self.basepath, entry)):
-                for item in prefix:
-                    if entry.startswith(item):
-                        print(entry)
-        print(f"Listing directories in ================ {self.basepath}")
+    def _has_dirs(self, path_to_dir):
+        abs_path = Path(path_to_dir).resolve()
+        with os.scandir(abs_path) as entries:
+            for entry in entries:
+                if entry.is_dir():
+                    return True
+        return False
 
-    def chooseFile(self, path_to_dir="", extension=None):
-        self.showOrderFilesByCTime(path_to_dir)
-        choosed_files = []
-        for entry in os.listdir(self.basepath):
-            if os.path.isfile(os.path.join(self.basepath, entry)):
+    def choose_file(self, path_to_dir, extension=None):
+        chosen_files = []
+        for entry in os.listdir(path_to_dir):
+            if os.path.isfile(os.path.join(path_to_dir, entry)):
                 if extension:
                     if entry.endswith(extension):
-                        choosed_files.append(entry)
+                        chosen_files.append(entry)
                 else:
-                    choosed_files.append(entry)
-        if len(choosed_files) == 0:
-            exit("[red]No files found")
+                    chosen_files.append(entry)
+        if len(chosen_files) == 0:
+            Print.error("No files found")
+            exit()
         else:
-            return Select.select_one(choosed_files)
+            return Select.select_one(chosen_files)
 
-    def appendToFile(self, file_path, text):
+    def append_to_file(self, file_path, text):
         with open(file_path, "a") as f:
             f.write(text)
         os.system(f"bat {file_path}")
 
-    def showOrderFilesByCTime(self, dir_path):
-        current_path = os.getcwd()
-        os.chdir(dir_path)
-        print(f"Listing files in ================ {dir_path}")
-
-        files = os.listdir()
-        # Collect (filename, ctime) tuples
-        file_ctimes = [(f, os.path.getctime(f)) for f in files if os.path.isfile(f)]
-
-        # Sort by ctime in reverse order
-        file_ctimes.sort(key=lambda x: x[1], reverse=True)
-
-        os.chdir(current_path)  # Restore original working directory
-
-        tb = MyTable()
-        tb_title = "Files sorted by creation time"
-        tb_headers = ["Id", "File name", "Created at"]
-        tb_rows = []
-
-        for i, (file, ctime) in enumerate(file_ctimes):
-            ctime_human = datetime.fromtimestamp(ctime).strftime("%Y-%m-%d %H:%M:%S")
-            tb_rows.append([i + 1, file, ctime_human])
-
-        tb.show(tb_title, tb_headers, tb_rows)
+    def select_multiple(self, options):
+        terminal_menu = TerminalMenu(
+            options,
+            multi_select=True,
+            show_multi_select_hint=True,
+            show_search_hint=True,
+            preview_command="bat --color=always {}",
+            preview_size=0.75,
+        )
+        terminal_menu.show()
+        return terminal_menu.chosen_menu_entries
